fd60b07f3f31c7cdcfd150f7f5627c70d94b6763
crjose <crjose1962@gmail.com>
2021-01-07 19:05:27 -0300

    Projeto Criado

--- Backend/src/test/java/com/devsuperior/dsdelliver/DsdelliverApplicationTests.java
+++ Backend/src/test/java/com/devsuperior/dsdelliver/DsdelliverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.dsdeliver;
+package com.devsuperior.dsdelliver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsdeliverApplicationTests {
+class DsdelliverApplicationTests {
 
 	@Test
 	void contextLoads() {
