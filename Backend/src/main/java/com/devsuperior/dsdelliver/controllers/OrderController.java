856cad9822fb996a5b2da90a7d4c6fd49c91501d
crjose <crjose1962@gmail.com>
2021-01-08 22:05:24 -0300

    PUT /orders/{id}/delivered

--- Backend/src/main/java/com/devsuperior/dsdelliver/controllers/OrderController.java
+++ Backend/src/main/java/com/devsuperior/dsdelliver/controllers/OrderController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,5 +54,17 @@ public class OrderController {
 		return ResponseEntity.created(uri).body(dto);
 		//fim da alteração atual
 	}
+	//complementando a rota depois do /orders
+	@PutMapping("/{id}/delivered")
+	//para que o id que está no parâmetro corresponda ao caminho "/{id} acima" 
+	//utilizamos a anotation @PathVariable
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
+		
+		
+	}
+	
 	
 }
--- Backend/src/main/java/com/devsuperior/dsdelliver/services/OrderService.java
+++ Backend/src/main/java/com/devsuperior/dsdelliver/services/OrderService.java
@@ -70,4 +70,17 @@ public class OrderService {
 				return new OrderDTO(order);
 				//fim da alteração atual
 	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+		
+		
+		
+		
+	}
 }
