2142a3b98f750e827021d2ebe01a92a7f30eeedb
crjose <crjose1962@gmail.com>
2021-01-08 21:32:29 -0300

    POST /Orders

--- Backend/src/main/java/com/devsuperior/dsdelliver/controllers/OrderController.java
+++ Backend/src/main/java/com/devsuperior/dsdelliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdelliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdelliver.dto.OrderDTO;
 import com.devsuperior.dsdelliver.services.OrderService;
@@ -17,10 +21,36 @@ public class OrderController {
 	
 	@Autowired OrderService service;
 	
+	//criando um endpoint para listar
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	//alteração atual
 
+	//criando um endpoint para salvar
+	
+	//a anotation @RequestBody servirá para o Spring transformar um Json em um objeto dto
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		//chamando o insert que acabamos de criar no service
+		dto = service.insert(dto);
+		//opção que funciona retornando o código 200
+		//return ResponseEntity.ok().body(dto);
+		
+		//opção adotada retornando o código 201 que significa: Recurso Criado,
+		//que passa o endereço da resposta usando um objeto do tipo URI de nome uri
+		//chamada que cria a URI corresponde ao recurso criado
+		//ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				//.buildAndExpand(dto.getId()).toUri();
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		//criando a resposta 201
+		return ResponseEntity.created(uri).body(dto);
+		//fim da alteração atual
+	}
+	
 }
--- Backend/src/main/java/com/devsuperior/dsdelliver/services/OrderService.java
+++ Backend/src/main/java/com/devsuperior/dsdelliver/services/OrderService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsdelliver.services;
 
-
-
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdelliver.dto.OrderDTO;
+import com.devsuperior.dsdelliver.dto.ProductDTO;
 import com.devsuperior.dsdelliver.entities.Order;
+import com.devsuperior.dsdelliver.entities.OrderStatus;
+import com.devsuperior.dsdelliver.entities.Product;
 import com.devsuperior.dsdelliver.repositories.OrderRepository;
-
-
+import com.devsuperior.dsdelliver.repositories.ProductRepository;
 
 /*Registrando o componente, pode ser de duas formas @Component ou @Service , 
 a opção pelo @Component @Service foi apenas por questões semânticas
@@ -21,20 +22,52 @@ a opção pelo @Component @Service foi apenas por questões semânticas
 
 @Service
 public class OrderService {
-	 
+
 	@Autowired
 	private OrderRepository repository;
-	
+
+	// alteração atual
+	@Autowired
+	private ProductRepository productRepository;
+	// fim dessa alteração
+
+	// buscando todos os pedidos do banco
 	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
-		//(alteração atual)
+	public List<OrderDTO> findAll() {
+
 		List<Order> list = repository.findOrdersWithProducts();
-		///List<Order> list = repository.findAll();
+		/// List<Order> list = repository.findAll();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 
+	// (alteração atual)
+	// INSERINDO os pedidos do banco
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		//instanciando um objeto do tipo Order e atribuindo o instante
+		//atual Instant.now() e o status do novo pedido 
+		//que será sempre PENDING(PENDENTE) OrderStatus.PENDING
+		
+		Order order = new Order(null, dto.getAddress() , dto.getLatitude() , dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+				
+				//Percorrendo o pedido  e buscando os produtos(ProductDTO)
+				//que vieram do dto
+				for(ProductDTO p : dto.getProducts()) {				
+//Associando o pedido(order) com os produtos que virão do dto adicionando-os 
+//pegando a lista de produtos(que nesse momento está vazia) e adicionando através do método add
+//como não se pode adicionar dto, adiciona-se a entidade product 
+//ao order(pedido), para isso usamos o método 
+						
+				Product product =  productRepository.getOne(p.getId());
+				order.getProducts().add(product);
+				}
+				//salvando no banco o objeto order, 
+				//o método save faz referência ao objeto salvo
+				order = repository.save(order);
+				//pegando a referência ao objeto order e passando como argumento de retorno
+				return new OrderDTO(order);
+				//fim da alteração atual
+	}
 }
-
-
-
-
